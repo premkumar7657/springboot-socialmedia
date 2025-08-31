fa8a58d412f183139af7e712a2f6e18d2dce16f7
premkumar7657 <83229009+premkumar7657@users.noreply.github.com>
2025-08-31 17:50:55 +0530

    Add main Spring Boot application class

---
+++ src/main/java/com/example/springbootsocialmedia/SpringbootSocialmediaApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootsocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSocialmediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSocialmediaApplication.class, args);
+    }
+}
